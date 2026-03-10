6c6939100671685012df9453ed4d1b8382a51c9f
cdubhenmorelikeCUBEHEN <140659150+baggZ-idk@users.noreply.github.com>
2026-03-10 14:37:02 -0400

    added BaGUI

---
+++ PCUtils/BaGUI.cs
@@ -0,0 +1,294 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace BaGUI
+{
+    public class Panel
+    {
+        public Vector2 Position;
+        public float Width = 300f;
+        public bool Draggable = true;
+        private bool dragging = false;
+        private Vector2 dragOffset;
+
+        public string Header;
+        public List<PanelItem> Items = new List<PanelItem>();
+
+        public Panel(string header, Vector2 position)
+        {
+            Header = header;
+            Position = position;
+        }
+        
+        float SquareSlider(Rect rect, float value, float min, float max)
+        {
+            float barHeight = 4f;
+            Rect bar = new Rect(rect.x, rect.y + rect.height / 2 - barHeight / 2, rect.width, barHeight);
+
+            GUI.color = new Color(0.3f, 0.3f, 0.3f, 1f);
+            GUI.DrawTexture(bar, Texture2D.whiteTexture);
+
+            float t = (value - min) / (max - min);
+            float handleX = rect.x + t * rect.width - 6;
+
+            Rect handle = new Rect(handleX, rect.y + rect.height / 2 - 6, 12, 12);
+
+            GUI.color = Color.white;
+            GUI.DrawTexture(handle, Texture2D.whiteTexture);
+
+            GUI.color = Color.white;
+
+            if (Event.current.type == EventType.MouseDrag || Event.current.type == EventType.MouseDown)
+            {
+                if (rect.Contains(Event.current.mousePosition))
+                {
+                    float mouseT = Mathf.InverseLerp(rect.x, rect.x + rect.width, Event.current.mousePosition.x);
+                    value = Mathf.Lerp(min, max, mouseT);
+                }
+            }
+
+            return Mathf.Clamp(value, min, max);
+        }
+        
+        bool SquareCheckbox(Rect rect, ref bool value, string label)
+        {
+            Rect box = new Rect(rect.x, rect.y + 4, 16, 16);
+
+            Color old = GUI.color;
+
+            GUI.color = value ? Color.white : new Color(0.35f, 0.35f, 0.35f, 1f);
+            GUI.DrawTexture(box, Texture2D.whiteTexture);
+            GUI.color = old;
+
+            GUI.Label(new Rect(rect.x + 22, rect.y, rect.width - 22, rect.height), label);
+
+            if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
+            {
+                value = !value;
+                return true;
+            }
+
+            return false;
+        }
+        
+        bool SolidButton(Rect rect, string label, Color color)
+        {
+            Color old = GUI.color;
+
+            GUI.color = color;
+            GUI.DrawTexture(rect, Texture2D.whiteTexture);
+
+            GUI.color = Color.white;
+
+            GUIStyle textStyle = new GUIStyle(GUI.skin.label)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 14,
+                normal = { textColor = Color.white }
+            };
+
+            GUI.Label(rect, label, textStyle);
+
+            GUI.color = old;
+
+            return Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition);
+        }
+
+        public void Draw()
+        {
+            GUIStyle headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 18,
+                fontStyle = FontStyle.Bold,
+                normal = { textColor = Color.white }
+            };
+
+            GUIStyle bodyStyle = new GUIStyle(GUI.skin.label)
+            {
+                normal = { textColor = Color.white },
+                wordWrap = true
+            };
+
+            GUIStyle buttonStyle = new GUIStyle(GUI.skin.button) { fontSize = 14 };
+            GUIStyle checkboxStyle = new GUIStyle(GUI.skin.toggle) { fontSize = 14, normal = { textColor = Color.white } };
+
+            Rect headerRect = new Rect(Position.x, Position.y, Width, 30);
+
+            if (Draggable && Event.current.type == EventType.MouseDown && headerRect.Contains(Event.current.mousePosition))
+            {
+                dragging = true;
+                dragOffset = Event.current.mousePosition - Position;
+            }
+
+            if (Event.current.type == EventType.MouseUp)
+                dragging = false;
+
+            if (dragging)
+                Position = Event.current.mousePosition - dragOffset;
+
+            float yOffset = 35f;
+            foreach (var item in Items)
+            {
+                if (item.Type == PanelItemType.Text)
+                    yOffset += bodyStyle.CalcHeight(new GUIContent(item.Label), Width - 10) + 5f;
+                else if (item.Type == PanelItemType.Button || item.Type == PanelItemType.Checkbox)
+                    yOffset += 30f;
+                else if (item.Type == PanelItemType.Slider)
+                    yOffset += 45f;
+                else if (item.Type == PanelItemType.Section)
+                {
+                    yOffset += 30f;
+                    if (!item.Collapsed)
+                        foreach (var sub in item.SubItems)
+                            yOffset += (sub.Type == PanelItemType.Text ? bodyStyle.CalcHeight(new GUIContent(sub.Label), Width - 20) :
+                                sub.Type == PanelItemType.Slider ? 45f : 30f) + 5f;
+                }
+            }
+
+            float panelHeight = yOffset + 10f;
+            Rect panelRect = new Rect(Position.x, Position.y, Width, panelHeight);
+
+            GUI.color = new Color(0.15f, 0.15f, 0.15f, 0.95f);
+            GUI.DrawTexture(panelRect, Texture2D.whiteTexture);
+            GUI.color = Color.white;
+
+            GUI.color = new Color(0.1f, 0.1f, 0.1f, 1f);
+            GUI.DrawTexture(headerRect, Texture2D.whiteTexture);
+            GUI.color = Color.white;
+
+            GUI.Label(new Rect(Position.x + 5, Position.y + 2, Width - 10, 30), Header, headerStyle);
+
+            float y = Position.y + 35f;
+            foreach (var item in Items)
+            {
+                switch (item.Type)
+                {
+                    case PanelItemType.Text:
+                    {
+                        float textHeight = bodyStyle.CalcHeight(new GUIContent(item.Label), Width - 10);
+                        GUI.Label(new Rect(Position.x + 5, y, Width - 10, textHeight), item.Label, bodyStyle);
+                        y += textHeight + 5f;
+                        break;
+                    }
+                    case PanelItemType.Button:
+                    {
+                        Rect btn = new Rect(Position.x + 5, y, Width - 10, 25);
+
+                        if (SolidButton(btn, item.Label, new Color(0.25f, 0.25f, 0.25f, 1f)))
+                            item.Action?.Invoke();
+                        y += 30f;
+                        break;
+                    }
+                    case PanelItemType.Checkbox:
+                    {
+                        SquareCheckbox(new Rect(Position.x + 5, y, Width - 10, 25), ref item.BoolValue, item.Label);
+                        y += 30f;
+                        break;
+                    }
+                    case PanelItemType.Slider:
+                    {
+                        GUI.Label(new Rect(Position.x + 5, y, Width - 10, 20),
+                            item.Label + " : " + item.FloatValue.ToString("0.00"), bodyStyle);
+                        item.FloatValue = SquareSlider(
+                            new Rect(Position.x + 5, y + 20, Width - 10, 20),
+                            item.FloatValue,
+                            item.Min,
+                            item.Max
+                        );
+                        break;
+                    }
+                    case PanelItemType.Section:
+                    {
+                        Rect btn = new Rect(Position.x + 5, y, Width - 10, 25);
+
+                        if (SolidButton(btn, item.Label + (item.Collapsed ? " [+]" : " [-]"),
+                                new Color(0.2f, 0.2f, 0.2f, 1f)))
+                            item.Collapsed = !item.Collapsed;
+                        y += 30f;
+
+                        if (!item.Collapsed)
+                            foreach (var sub in item.SubItems)
+                            {
+                                switch (sub.Type)
+                                {
+                                    case PanelItemType.Text:
+                                    {
+                                        float subHeight = bodyStyle.CalcHeight(new GUIContent(sub.Label), Width - 20);
+                                        GUI.Label(new Rect(Position.x + 15, y, Width - 20, subHeight), sub.Label,
+                                            bodyStyle);
+                                        y += subHeight + 5f;
+                                        break;
+                                    }
+
+                                    case PanelItemType.Button:
+                                    {
+                                        Rect subBtn = new Rect(Position.x + 15, y, Width - 20, 25);
+
+                                        if (SolidButton(subBtn, sub.Label, new Color(0.25f, 0.25f, 0.25f, 1f)))
+                                            sub.Action?.Invoke();
+                                        y += 30f;
+                                        break;
+                                    }
+                                    case PanelItemType.Checkbox:
+                                    {
+                                        SquareCheckbox(new Rect(Position.x + 15, y, Width - 20, 25), ref sub.BoolValue, sub.Label);
+                                        y += 30f;
+                                        break;
+                                    }
+                                    case PanelItemType.Slider:
+                                    {
+                                        GUI.Label(new Rect(Position.x + 15, y, Width - 20, 20),
+                                            sub.Label + " : " + sub.FloatValue.ToString("0.00"), bodyStyle);
+                                        sub.FloatValue = SquareSlider(
+                                            new Rect(Position.x + 15, y + 20, Width - 20, 20),
+                                            sub.FloatValue,
+                                            sub.Min,
+                                            sub.Max
+                                        );
+                                        y += 45f;
+                                        break;
+                                    }
+                                }
+                            }
+                    }
+                        break;
+                }
+            }
+        }
+    }
+
+    public enum PanelItemType { Text, Button, Checkbox, Slider, Section }
+
+    public class PanelItem
+    {
+        public PanelItemType Type;
+        public string Label;
+        public Action Action;
+
+        public bool BoolValue;
+
+        public float FloatValue;
+        public float Min;
+        public float Max;
+
+        public bool Collapsed = true;
+        public List<PanelItem> SubItems = new List<PanelItem>();
+
+        public PanelItem(string label, PanelItemType type, Action action = null)
+        {
+            Label = label;
+            Type = type;
+            Action = action;
+        }
+
+        public PanelItem(string label, float min, float max, float defaultValue)
+        {
+            Label = label;
+            Type = PanelItemType.Slider;
+            Min = min;
+            Max = max;
+            FloatValue = defaultValue;
+        }
+    }
+}
--- PCUtils/Plugin.cs
+++ PCUtils/Plugin.cs
@@ -3,6 +3,7 @@ using MelonLoader;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using GorillaLocomotion;
+using BaGUI;
 
 [assembly: MelonInfo(typeof(Plugin), PCUtils.Constants.ModName, PCUtils.Constants.Version, PCUtils.Constants.ModAuthor)]
 [assembly: MelonGame("Another Axiom", "Gorilla Tag")]
@@ -12,9 +13,14 @@ namespace PCUtils
     {
         private LayerMask coolLayers;
         private GorillaTriggerColliderHandIndicator clickyCollider;
-        
+
         public bool isActive = false;
+        public bool helpMenu = false;
         
+        private Panel utilsPanel;
+        private PanelItem sensitivitySlider;
+        private PanelItem firstPersonToggle;
+
         public override void OnInitializeMelon()
         {
             GorillaTagger.OnPlayerSpawned(OnPlayerSpawn);
@@ -26,31 +32,46 @@ namespace PCUtils
                 .GetComponent<GorillaTriggerColliderHandIndicator>();
 
             coolLayers = 1 << 18;
+            
+            sensitivitySlider = new PanelItem("Sensitivity", 0f, 2f, 1f);
+            firstPersonToggle = new PanelItem("First Person", PanelItemType.Checkbox);
+        }
+
+        public override void OnUpdate()
+        {
+            if (Keyboard.current.tabKey.wasPressedThisFrame)
+                isActive = !isActive;
+            
+            string gameMode = NetworkSystem.Instance.InRoom
+                ? GorillaLibrary.GameModes.Utilities.GameModeUtility.CurrentGamemode.ID
+                : GorillaLibrary.GameModes.Constants.ModdedPrefix;
+
+            if (!isActive || !gameMode.StartsWith(GorillaLibrary.GameModes.Constants.ModdedPrefix))
+                return;
+            
+            if (Keyboard.current.hKey.wasPressedThisFrame && isActive)
+                helpMenu = !helpMenu;
+            
+            GorillaTagger.Instance.thirdPersonCamera.transform.GetChild(0).GetComponent<Camera>().gameObject.SetActive(!firstPersonToggle.BoolValue);
         }
 
         public override void OnFixedUpdate()
         {
+            string gameMode = NetworkSystem.Instance.InRoom
+                ? GorillaLibrary.GameModes.Utilities.GameModeUtility.CurrentGamemode.ID
+                : GorillaLibrary.GameModes.Constants.ModdedPrefix;
 
-            if (Keyboard.current.tabKey.wasPressedThisFrame)
-            {
-                isActive = !isActive;
-                VRRig.LocalRig.head.trackingRotationOffset = Vector3.zero;
-            }
-
-            string gameMode = NetworkSystem.Instance.InRoom ? GorillaLibrary.GameModes.Utilities.GameModeUtility.CurrentGamemode.ID : GorillaLibrary.GameModes.Constants.ModdedPrefix;
-            
             if (!isActive || !gameMode.StartsWith(GorillaLibrary.GameModes.Constants.ModdedPrefix))
                 return;
-            
+
             Transform head = GorillaTagger.Instance.headCollider.transform;
-            Transform body = GorillaTagger.Instance.bodyCollider.transform;
 
             if (Mouse.current.rightButton.isPressed)
             {
                 Vector2 mouse = Mouse.current.delta.ReadValue();
-                head.Rotate(Vector3.up,    mouse.x  * 0.08f, Space.World);
-                head.Rotate(Vector3.right, -mouse.y * 0.08f, Space.Self);
-                
+                head.Rotate(Vector3.up, mouse.x * 0.08f  * sensitivitySlider.FloatValue, Space.World);
+                head.Rotate(Vector3.right, -mouse.y * 0.08f  * sensitivitySlider.FloatValue, Space.Self);
+
                 VRRig.LocalRig.head.rigTarget.transform.rotation = head.rotation;
 
                 Cursor.lockState = CursorLockMode.Locked;
@@ -62,21 +83,22 @@ namespace PCUtils
 
             Vector3 direction = Vector3.zero;
 
-            if (Keyboard.current.wKey.isPressed) direction += body.forward;
-            if (Keyboard.current.sKey.isPressed) direction -= body.forward;
-            if (Keyboard.current.aKey.isPressed) direction -= body.right;
-            if (Keyboard.current.dKey.isPressed) direction += body.right;
-            if (Keyboard.current.spaceKey.isPressed) direction += body.up;
-            if (Keyboard.current.leftCtrlKey.isPressed) direction -= body.up;
+            if (Keyboard.current.wKey.isPressed) direction += head.forward;
+            if (Keyboard.current.sKey.isPressed) direction -= head.forward;
+            if (Keyboard.current.aKey.isPressed) direction -= head.right;
+            if (Keyboard.current.dKey.isPressed) direction += head.right;
+            if (Keyboard.current.spaceKey.isPressed) direction += head.up;
+            if (Keyboard.current.leftCtrlKey.isPressed) direction -= head.up;
 
             Rigidbody playerRB = GorillaTagger.Instance.rigidbody;
-            
-            playerRB.transform.position += direction.normalized * Time.fixedDeltaTime * (Keyboard.current.leftShiftKey.isPressed ? 30f : 10f);
+
+            playerRB.transform.position += direction.normalized * Time.fixedDeltaTime *
+                                           (Keyboard.current.leftShiftKey.isPressed ? 30f : 10f);
             playerRB.linearVelocity = Vector3.zero;
             playerRB.AddForce(-Physics.gravity * playerRB.mass);
-            
+
             Camera thirdPerson = GorillaTagger.Instance.thirdPersonCamera.transform.GetChild(0).GetComponent<Camera>();
-            
+
             if (Mouse.current.leftButton.isPressed)
             {
                 Camera camera = thirdPerson.gameObject.activeInHierarchy ? thirdPerson : GTPlayer.Instance.mainCamera;
@@ -85,14 +107,48 @@ namespace PCUtils
                         25f, coolLayers))
                     return;
 
-                clickyCollider.transform.position = hit.point;   
+                clickyCollider.transform.position = hit.point;
             }
-            
-            if (Keyboard.current.cKey.wasPressedThisFrame && isActive)
-                thirdPerson.gameObject.SetActive(!thirdPerson.gameObject.activeInHierarchy);
-            
+
             if (Keyboard.current.escapeKey.wasPressedThisFrame && isActive)
                 NetworkSystem.Instance.ReturnToSinglePlayer();
+        }
+
+        public override void OnGUI()
+        {
+            if (!helpMenu || !isActive)
+                return;
+
+            if (utilsPanel == null)
+            {
+                utilsPanel = new Panel("PCUtils", new Vector2(20, 20));
+                
+                var helpSection = new PanelItem("Help", PanelItemType.Section);
+
+                helpSection.SubItems.Add(new PanelItem("Tab - Toggles mod", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("Esc - Leaves the room", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("C - Toggles first person", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("H - Toggles help menu", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("Right Click - Look around", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("Left Click - Click buttons (most mods are incompatible with this feature)", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("W - Move forwards", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("A - Move left", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("S - Move backwards", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("D - Move right", PanelItemType.Text));
+                helpSection.SubItems.Add(new PanelItem("Shift - Sprint", PanelItemType.Text));
+                
+                utilsPanel.Items.Add(helpSection);
+                
+                
+                var settingsSection = new PanelItem("Settings", PanelItemType.Section);
+                
+                settingsSection.SubItems.Add(sensitivitySlider);
+                settingsSection.SubItems.Add(firstPersonToggle);
+                
+                utilsPanel.Items.Add(settingsSection);
+            }
+
+            utilsPanel.Draw();
         }
     }
 }
