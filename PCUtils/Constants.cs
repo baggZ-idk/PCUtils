40e4803649e1596b4048e49b1e1820f3e83c3c19
cdubhenmorelikeCUBEHEN <140659150+baggZ-idk@users.noreply.github.com>
2026-03-14 12:05:09 -0400

    change version number

--- PCUtils/Constants.cs
+++ PCUtils/Constants.cs
@@ -2,7 +2,7 @@
 {
     internal class Constants
     {
-        public const string Version = "1.2.0";
+        public const string Version = "1.2.1";
         public const string ModName = "PCUtils";
         public const string ModAuthor = "baggZ";
     }
